901ede9aacefd1181afaeaec92c92d69da5b8aae
Francisco Timez <franciscotimez@gmail.com>
2023-11-16 10:20:17 -0300

    contador con observable

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,30 @@
 import { Observable, Observer } from "rxjs";
 
 const observer: Observer<any> = {
-  next: function (value: any): void {
-    console.log("[next]:", value);
-  },
-  error: function (err: any): void {
-    console.warn("[error]:", err);
-  },
-  complete: function (): void {
-    console.info("[complete]:");
-  }
+  next: (value: any): void => console.log("[next]:", value),
+  error: (err: any): void => console.warn("[error]:", err),
+  complete: (): void => console.info("[complete]:")
 };
+
+
+// Crear un contador que emita cada segundo 1,2,3,4,5...
+const intervalo$ = new Observable<number>(subs => {
+  let count = 0;
+  const interval = setInterval(() => {
+    count++;
+    console.log("Intervalo", count);
+    subs.next(count);
+  }, 1000);
+
+  // Funcion que se ejecuta en el unsubscribe
+  return () => {
+    clearInterval(interval);
+    console.log("Intervalo Destruido");
+  };
+});
+
+const subscription = intervalo$.subscribe(num => console.log("Num: ", num));
+
+setTimeout(() => {
+  subscription.unsubscribe();
+}, 3000);
